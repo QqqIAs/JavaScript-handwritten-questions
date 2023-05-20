2aec81310a176c14a2a8256adf4ad3ea9a8df6f1
QqqIAs <1982421402@qq.com>
2023-05-20 16:02:38 +0800

    fix

--- ts复习/字面量类型.ts
+++ ts复习/字面量类型.ts
@@ -10,3 +10,15 @@ function request(url: string, method: 'GET'|'POST') {
 }
 
 request(info.url, info.method) //类型“string”的参数不能赋给类型“"GET" | "POST"”的参数。
+
+//解决方法
+//1
+request(info.url, info.method as 'GET')  //断言
+
+//2
+let newinfo = {
+  url: 'http://123.com',
+  method: 'GET'
+} as const
+
+request(newinfo.url, newinfo.method)
