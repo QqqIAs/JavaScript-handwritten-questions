9ca43c2ff9f38ebce305700896a06cf02e785e7a
QqqIAs <QqqIAs0105@163.com>
2023-06-16 02:25:21 +0800

    test: 新终端

--- ts复习/交叉类型.ts
+++ ts复习/交叉类型.ts
@@ -6,6 +6,8 @@
  * @FilePath: \手写\手写题\ts复习\交叉类型.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+
+//zh
 interface Colorful {
   color: string;
 }
